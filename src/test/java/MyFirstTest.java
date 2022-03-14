73852ee6c58f16de3634b03487f38eea2905a728
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-14 14:24:11 -0500

    working through exercises

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,4 +1,31 @@
+import org.junit.Test;
+import org.junit.Before;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class MyFirstTest {
 
+    @Test
+    public void testIfStringsAreEqual() {
+        String string1 = "Codeup";
+        String string2 = "codeup";
 
-}
+        assertEquals(string1, string2);
+    }
+
+
+    @Test
+    public void testIfArraysAreNotEqual(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+
+
+    }
+
+
+
+}//END OF CLASS
