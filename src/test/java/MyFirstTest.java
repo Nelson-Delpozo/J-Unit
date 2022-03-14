62371358cba8339c39da1c6ad430f5c650feffe8
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-14 14:32:15 -0500

    finished warmups

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -22,9 +22,32 @@ public class MyFirstTest {
         List<String> moreLanguages = new ArrayList<>();
 
         assertNotSame(languages, moreLanguages);
+    }
 
+    @Test
+    public void compareArrays(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
 
     }
+
+    @Test
+    public void assertTrueAndFalse(){
+        String language = "PHP";
+        language.contains("H"); // use assertTrue for this statement
+        language.contains("J"); // use assertFalse for this statement
+
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
+    }
+
+
+
 
 
 
