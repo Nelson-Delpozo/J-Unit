663ce58c2cc6e7ee92f83a21279dd3c158529daf
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-14 11:59:17 -0500

    added second test

--- src/test/java/jUnitTests.java
+++ src/test/java/jUnitTests.java
@@ -11,6 +11,18 @@ public class jUnitTests {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+
+
 
 
     public static void main(String[] args) {
