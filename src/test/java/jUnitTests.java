df0bcdf695398cf88b3a90b82d81a595d1c4dd25
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-14 11:58:07 -0500

    added first test

--- src/test/java/jUnitTests.java
+++ src/test/java/jUnitTests.java
@@ -3,5 +3,17 @@ import static org.junit.Assert.*;
 
 public class jUnitTests {
 
+    @Test
+    public void testIfNameIsNelson(){
+        String expected = "Nelson";
+        String actual = "Cindy";
 
-}
+        assertEquals(expected, actual);
+    }
+
+
+
+    public static void main(String[] args) {
+    }//END OF MAIN
+
+}//END OF CLASS
