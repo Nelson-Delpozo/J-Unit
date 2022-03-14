ed22f6d6a0f1458cea37b034f94f1260bb0286d8
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-14 14:17:27 -0500

    warmup exercises

--- src/test/java/jUnitTests.java
+++ src/test/java/jUnitTests.java
@@ -1,4 +1,9 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class jUnitTests {
@@ -38,6 +43,50 @@ public class jUnitTests {
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+//    @Test
+//    public void testIfInstanceIsNull() {
+//        Device phone = new Device();
+//        Device laptop = null;
+//        assertNull(null);
+//        assertNotNull(phone);
+//    }
+
+    private List<String> names;
+
+    @Before
+    public void setUp(){
+        this.names = new ArrayList<>();
+        this.names.add("Fer");
+    }
+
+    // We can verify that the names list is not longer null because it was initialized in the setUp() method with the following test:
+    @Test
+    public void testIfNamesIsInitialized(){
+        assertNotNull(names);
+    }
+
+    @Test
+    public void testIfANameCanBeAdded(){
+        assertEquals(1, this.names.size());
+        this.names.add("Zach");
+        assertEquals(2, this.names.size());
+        assertSame("fer", this.names.get(0));
+        assertSame("Zach", this.names.get(1));
+    }
+
+
+
 
 
 
