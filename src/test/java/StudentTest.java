3632912bbba383a1cc73d0b20aa0fbac71dec243
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-15 09:07:55 -0500

    morning commit

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 public class StudentTest {
+
+
+
+
+
 
 }
